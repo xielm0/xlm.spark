992f6d3339f9cf2c8495c90cdc929b1ab6682cd8
xieliming <xieliming@jd.com>
2016-06-03 15:46:06 +0800

    spark_demo

---
+++ spark_demo/src/main/python/LoadHive.py
@@ -0,0 +1,22 @@
+# A simple hive demo. If you do not have a table to load from look run MakeHiveTable.py
+from pyspark import SparkContext
+from pyspark.sql import HiveContext
+import json
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print "Error usage: LoadHive [sparkmaster] [inputtable]"
+        sys.exit(-1)
+    master = sys.argv[1]
+    inputTable = sys.argv[2]
+    sc = SparkContext(master, "LoadHive")
+    hiveCtx = HiveContext(sc)
+    # Query hive
+    input = hiveCtx.hql("FROM " + inputTable + " SELECT key, value")
+    data = input.map(lambda x: x['key'] * x['key'])
+    result = data.collect()
+    for element in result:
+        print "Got data " + str(element)
+    sc.stop()
+    print "Done!"
