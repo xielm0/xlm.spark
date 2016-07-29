c075e066683888119937fda8b2e7c69179717914
xieliming <xieliming@jd.com>
2016-07-29 18:07:35 +0800

    dmp_intrest

--- dmp_intrest/src/main/bin/intrest_train_jdpin.sh
+++ dmp_intrest/src/main/bin/intrest_train_jdpin.sh
@@ -9,11 +9,11 @@ dir='/home/jd_ad/spark_task/dmp_intrest'
 cd ${dir}
 
  #spark-submit --master yarn-cluster --queue bdp_jmart_adv.bdp_jmart_sz_ad \
- # --conf spark.dynamicAllocation.enabled=true  --conf spark.shuffle.service.enabled=true --conf spark.dynamicAllocation.maxExecutors=100 \
+
  spark-submit --master yarn-client  \
  --driver-memory 4g \
- --num-executors 50 \
- --executor-memory 8g \
+ --conf spark.dynamicAllocation.enabled=true  --conf spark.shuffle.service.enabled=true --conf spark.dynamicAllocation.maxExecutors=100 \
+ --executor-memory 12g \
  --executor-cores 4 \
  --class com.jd.szad.intrest.lda_jd  dmp_intrest.jar  \
   train  22 36 1.3 1.05 app.db/app_szad_m_dmp_label_intrest_jdpin_train  800 app.db/app_szad_m_dmp_label_intrest_res_jdpin
