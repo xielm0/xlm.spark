38644e65091550c435f05e36ed529cb17aa0412e
xieliming <xieliming@jd.com>
2017-03-10 11:49:30 +0800

    dyrec

--- dyrec/src/main/bin/xielm/userlabel_pre_1_short_cate2click.sh
+++ dyrec/src/main/bin/xielm/userlabel_pre_1_short_cate2click.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 dt=`date -d last-day +%Y-%m-%d`
 ftime=`date -d last-day +%Y%m%d`
@@ -6,18 +6,16 @@ echo ${ftime}
 queue=bdp_jmart_adv.bdp_jmart_sz_ad
 echo ${dt}
 
+
 spark-submit --master yarn-client \
  --driver-memory 6g \
  --num-executors 80 \
  --executor-memory 16g \
  --executor-cores 8 \
  --class com.jd.szad.userlabel.app dyrec.jar \
- predict2 browse_top20brand 7_click_top20brand app.db/app_szad_m_dyrec_userlabel_predict_res2/user_type=1/type=7_click_top20brand
-
-#predict 1=1
+ predict2 browse_top20cate 7_click_top20cate app.db/app_szad_m_dyrec_userlabel_predict_res2/user_type=1/type=7_click_top20cate
 
 
 
-
-
-
+#spark2.0
+#source /data/bossapp/software/bashrc
