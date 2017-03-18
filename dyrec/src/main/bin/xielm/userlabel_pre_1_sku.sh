14c57f1bac1788f95496ff4dd2b0b1d535fcb6a9
xieliming <xieliming@jd.com>
2017-03-18 14:19:59 +0800

    dyrec

--- dyrec/src/main/bin/xielm/userlabel_pre_1_sku.sh
+++ dyrec/src/main/bin/xielm/userlabel_pre_1_sku.sh
@@ -10,7 +10,7 @@ echo ${dt}
 spark-submit --master yarn-client \
  --driver-memory 6g \
  --num-executors 50 \
- --executor-memory 16g \
- --executor-cores 8 \
+ --executor-memory 10g \
+ --executor-cores 5 \
  --class com.jd.szad.userlabel.app dyrec.jar \
  predict2 sku sku app.db/app_szad_m_dyrec_userlabel_predict_res2/user_type=1/type=sku
--- dyrec/src/main/scala/com/jd/szad/userlabel/app.scala
+++ dyrec/src/main/scala/com/jd/szad/userlabel/app.scala
@@ -37,12 +37,12 @@ object app {
         """.stripMargin
       val df = sqlContext.sql(sql).persist(StorageLevel.MEMORY_AND_DISK)
 
-
       // type,label,sku
       val df1 = df.groupBy("type", "label", "sku").agg(sum("rate") as "sku_uv").filter(col("sku_uv") > 10)
-      val df2 = df.groupBy("type", "label").agg(countDistinct("uid") as "label_uv").filter(col("label_uv") > 10)
+//      val df2 = df.groupBy("type", "label").agg(countDistinct("uid") as "label_uv").filter(col("label_uv") > 10)
+      val df3 = df.groupBy("type","sku").agg(countDistinct("uid") as "sku_all_uv").filter(col("sku_all_uv") > 10)
 
-      val label_sku =  df1.join(df2, Seq("type", "label")).selectExpr("type", "label", "sku", "round(sku_uv/log(1+label_uv),4) as score")
+      val label_sku =  df1.join(df3, Seq("type", "sku")).selectExpr("type", "label", "sku", "round(sku_uv/log(1+sku_all_uv),4) as score")
 
       //save
 //      val res = label_sku.rdd.map(t=>t.getAs("type").toString + "\t" + t.getAs("label") + "\t" + t.getAs("sku") + "\t" + t.getAs("score"))
@@ -54,6 +54,65 @@ object app {
       sqlContext.sql("set mapred.output.compression.codec=com.hadoop.compression.lzo.LzopCodec")
 
       sqlContext.sql("insert overwrite table app.app_szad_m_dyrec_userlabel_model select type,label,sku,score from res_table where label <> String(sku)")
+
+    } else if (model_type =="predict2") {
+      // 数据倾斜时
+
+      val cond1 = args(1).toString
+      val cond2 = args(2).toString
+      val output_path = args(3)
+      val partitions =500
+      sqlContext.sql(s"set spark.sql.shuffle.partitions = ${partitions}")
+
+      val sql1 =
+        s"""
+           |select uid,label, rate
+           |from app.app_szad_m_dyrec_userlabel_apply
+           |where user_type=1
+           |and length(uid)>20
+           |and  type='${cond1}'
+        """.stripMargin
+      val df_user_label = sqlContext.sql(sql1)
+
+      val sql2 =
+        s"""
+           |select label,sku,score
+           |  from (select sku,type,label,score,row_number() over(partition by type,label order by score desc ) rn
+           |         from app.app_szad_m_dyrec_userlabel_model
+           |        where type='${cond2}'
+           |          and label <> String(sku)  )t
+           |where rn<=20
+        """.stripMargin
+      val df_label_sku = sqlContext.sql(sql2)
+
+      val t2 = df_label_sku.rdd.map(t=>(t.getAs("label").toString,(t.getAs("sku").toString,t.getAs("score").asInstanceOf[Double]))).groupByKey().collectAsMap()
+      val bc_t2 = sc.broadcast(t2)
+
+      val t0 = df_user_label.groupBy("label").agg(countDistinct("uid") as "label_uv")
+      val tt = df_user_label.join(t0,"label").selectExpr("uid","label","round(rate/log(1+label_uv),4) as rate2")
+      val t1 = tt.rdd.map(t=>(t.getAs("uid").toString,t.getAs("label").toString,t.getAs("rate").asInstanceOf[Double]))
+
+      val rdd_join =t1.mapPartitions{iter =>
+        for{(uid,label,rate) <- iter
+            if (bc_t2.value.contains(label))
+            skus= bc_t2.value.get(label)
+            sku <- skus.get
+        }  yield (uid, sku._1, math.round(sku._2 * math.sqrt(rate) *10000)/10000.0 )
+      }
+
+      //top 100
+      val k =20
+      val top100 = rdd_join.map(t=> (t._1,(t._2,t._3))).groupByKey().flatMap{
+        case( a, b)=>  //b=Interable[(sku,score)]
+          val bb = b.toBuffer
+          val topk =bb.sortBy(_._2).reverse
+          if (topk.length >k ) topk.remove( k, topk.length-k)
+          topk.zipWithIndex.map{case((sku,score),rn) => (a, sku,score,rn) }
+      }
+
+      //save
+      val res = top100.map(t=>t._1 + "\t" + t._2.toString + "\t" + t._3.toString + "\t" + t._4 )
+      Writer.write_table( res ,output_path,"lzo")
 
     } else if (model_type =="predict") {
       // 无数据倾斜时
@@ -86,7 +145,7 @@ object app {
 
       // join
       val partitions=1000
-//      val k =50
+      //      val k =50
       sqlContext.sql(s"set spark.sql.shuffle.partitions = ${partitions}")
 
       //df join
@@ -100,61 +159,6 @@ object app {
 
       //save
       val res = df4.rdd.map(t => t.getAs("uid").toString + "\t" + t.getAs("sku") + "\t" + t.getAs("score") + "\t" + t.getAs("rn") )
-      Writer.write_table( res ,output_path,"lzo")
-
-    }else if (model_type =="predict2") {
-      // 数据倾斜时
-
-      val cond1 = args(1).toString
-      val cond2 = args(2).toString
-      val output_path = args(3)
-      val partitions =500
-
-      val sql1 =
-        s"""
-           |select uid,label, rate
-           |from app.app_szad_m_dyrec_userlabel_apply
-           |where user_type=1
-           |and length(uid)>20
-           |and  type='${cond1}'
-        """.stripMargin
-      val df_user_label = sqlContext.sql(sql1)
-
-      val sql2 =
-        s"""
-           |select label,sku,score
-           |  from (select sku,type,label,score,row_number() over(partition by type,label order by score desc ) rn
-           |         from app.app_szad_m_dyrec_userlabel_model
-           |        where type='${cond2}'
-           |          and label <> String(sku)  )t
-           |where rn<=20
-        """.stripMargin
-      val df_label_sku = sqlContext.sql(sql2)
-
-      val t2 = df_label_sku.rdd.map(t=>(t.getAs("label").toString,(t.getAs("sku").toString,t.getAs("score").asInstanceOf[Double]))).groupByKey().collectAsMap()
-      val bc_t2 = sc.broadcast(t2)
-      val t1 = df_user_label.rdd.map(t=>(t.getAs("uid").toString,t.getAs("label").toString,t.getAs("rate").asInstanceOf[Int])).repartition(partitions)
-
-      val rdd_join =t1.mapPartitions{iter =>
-        for{(uid,label,rate) <- iter
-            if (bc_t2.value.contains(label))
-            skus= bc_t2.value.get(label)
-            sku <- skus.get
-        }  yield (uid, sku._1, math.round(sku._2 * math.sqrt(rate) *10000)/10000.0 )
-      }
-
-      //top 100
-      val k =20
-      val top100 = rdd_join.map(t=> (t._1,(t._2,t._3))).groupByKey().flatMap{
-        case( a, b)=>  //b=Interable[(sku,score)]
-          val bb = b.toBuffer
-          val topk =bb.sortBy(_._2).reverse
-          if (topk.length >k ) topk.remove( k, topk.length-k)
-          topk.zipWithIndex.map{case((sku,score),rn) => (a, sku,score,rn) }
-      }
-
-      //save
-      val res = top100.map(t=>t._1 + "\t" + t._2.toString + "\t" + t._3.toString + "\t" + t._4 )
       Writer.write_table( res ,output_path,"lzo")
 
     }
